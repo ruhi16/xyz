1c09a8effaf75342def1f1d3ae4434273263cb4f
Hari Narayan Das <hndas16@gmail.com>
2017-07-29 12:21:03 +0530

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -29,7 +29,7 @@ class AdminController extends Controller
 			$user->save();
 
 	   		session()->put('error','Successfully User Registered.');
-	    	return redirect()->to('/home');
+	    	return redirect()->to('/login');
 
     	}else{
     		session()->put('error','Password does not matched!');
